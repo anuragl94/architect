c2aa66e833e3bcecddd11bef89924eae312f81db
Anurag Lingam <anuten8@gmail.com>
2018-03-11 02:54:09 +0530

    Basic ExtendedHTML component 'Arc'

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import Arc from './src'
 
 class XButton extends Arc {
+  static get properties () {
+    return {
+      'disabled': { type: Boolean }
+    }
+  }
+  reconcile (dom, prevState, nextState) {
+    if (prevState.disabled === nextState.disabled) {
+      return true
+    }
+    let button = dom.querySelector('button')
+    nextState.disabled
+      ? button.setAttribute('disabled', '')
+      : button.removeAttribute('disabled')
+  }
   render () {
+    console.warn('Entire tree rendered')
+    let container = document.createElement('span')
     let button = document.createElement('button')
     button.innerText = 'Test success'
-    return button
+    this.getAttribute('disabled') === ''
+      ? button.setAttribute('disabled', '')
+      : button.removeAttribute('disabled')
+    container.appendChild(button)
+    return container
   }
 }
 
